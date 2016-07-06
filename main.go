4117a579ccc57167798d498fa1e1b02105a54794
munisystem <munisystem@gmail.com>
2016-07-06 17:19:35 +0900

    Read model

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -16,18 +19,62 @@ func main() {
 	dir := os.Args[1]
 
 	files, err := ioutil.ReadDir(dir)
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	ifErr(err)
 
 	for _, file := range files {
 		if file.IsDir() {
 			continue
 		}
 
-		filePath := filepath.Join(dir, file.Name())
-		fmt.Println(filePath)
+		path := filepath.Join(dir, file.Name())
+		fmt.Println(path)
+
+		generateController(path)
+	}
+}
+
+func generateController(path string) {
+	structName, err := modelName(path)
+	ifErr(err)
+	fmt.Println("ModelName: " + structName)
+}
+
+func modelName(path string) (string, error) {
+	fset := token.NewFileSet()
+
+	f, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
+	ifErr(err)
+
+	var structName string
+
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.GenDecl:
+			if x.Tok != token.TYPE {
+				break
+			}
+			for _, spec := range x.Specs {
+				typeSpec, ok := spec.(*ast.TypeSpec)
+				if !ok {
+					continue
+				}
+
+				_, ok = typeSpec.Type.(*ast.StructType)
+				if !ok {
+					continue
+				}
+
+				structName = typeSpec.Name.Name
+			}
+		}
+		return true
+	})
+	return structName, nil
+}
+
+func ifErr(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
