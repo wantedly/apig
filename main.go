a11a5ac6f1663dcbbc46056080cd0ad22e2441f1
Daisuke Fujita <dtanshi45@gmail.com>
2016-07-06 11:45:58 +0900

    Show fields

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 	"path/filepath"
 )
 
+type Model struct {
+	Name   string
+	Fields map[string]string
+}
+
 func parseFile(path string) error {
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, path, nil, 0)
@@ -18,7 +23,72 @@ func parseFile(path string) error {
 		return err
 	}
 
-	ast.Print(fset, f)
+	models := []*Model{}
+
+	ast.Inspect(f, func(node ast.Node) bool {
+		switch x := node.(type) {
+		case *ast.GenDecl:
+			if x.Tok != token.TYPE {
+				break
+			}
+
+			for _, spec := range x.Specs {
+				fieldNames := []string{}
+				fields := make(map[string]string)
+
+				var modelName string
+
+				switch x2 := spec.(type) {
+				case *ast.TypeSpec:
+					modelName = x2.Name.Name
+
+					switch x3 := x2.Type.(type) {
+					case *ast.StructType:
+						for _, field := range x3.Fields.List {
+							for _, name := range field.Names {
+								fieldNames = append(fieldNames, name.Name)
+							}
+
+							var fieldType string
+
+							switch x4 := field.Type.(type) {
+							case *ast.Ident: // e.g. string
+								fieldType = x4.Name
+							case *ast.StarExpr: // e.g. *time.Time
+								switch x5 := x4.X.(type) {
+								case *ast.SelectorExpr:
+									switch x6 := x5.X.(type) {
+									case *ast.Ident:
+										fieldType = x6.Name + "." + x5.Sel.Name
+									}
+								}
+							}
+
+							for _, name := range fieldNames {
+								fields[name] = fieldType
+							}
+						}
+					}
+
+					models = append(models, &Model{
+						Name:   modelName,
+						Fields: fields,
+					})
+				}
+			}
+		}
+
+		return true
+	})
+
+	for _, model := range models {
+		fmt.Println("Model: " + model.Name)
+		fmt.Println("Fields: ")
+
+		for name, t := range model.Fields {
+			fmt.Println("  - " + name + " => " + t)
+		}
+	}
 
 	return nil
 }
