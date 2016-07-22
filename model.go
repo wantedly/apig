128c5f075f422c40bdea797cbfbd2a885f1fe71e
Daisuke Fujita <dtanshi45@gmail.com>
2016-07-22 11:52:08 +0900

    Load nested fields

--- model.go
+++ model.go
@@ -12,12 +12,42 @@ type Model struct {
 	Fields []*Field
 }
 
+func (m *Model) AllPreloadAssocs() []string {
+	result := []string{}
+
+	for _, field := range m.Fields {
+		result = append(result, field.PreloadAssocs()...)
+	}
+
+	return result
+}
+
 type Field struct {
 	Name        string
 	JSONName    string
 	Type        string
 	Tag         string
 	Association *Association
+}
+
+func (f *Field) PreloadAssocs() []string {
+	if f.Association == nil || f.Association.Type == AssociationNone {
+		return []string{}
+	}
+
+	result := []string{
+		f.Name,
+	}
+
+	for _, field := range f.Association.Model.Fields {
+		if field.Association == nil || field.Association.Type == AssociationNone {
+			continue
+		}
+
+		result = append(result, f.Name+"."+field.Name)
+	}
+
+	return result
 }
 
 func (f *Field) IsAssociation() bool {
