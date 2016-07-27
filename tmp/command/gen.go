6051a56929e554392b80f16dc69b0e396d9ea9fe
munisystem <munisystem@gmail.com>
2016-07-27 16:50:19 +0900

    Add support for gen subcommand

--- tmp/command/gen.go
+++ tmp/command/gen.go
@@ -1,7 +1,18 @@
 package command
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
+
+	"github.com/wantedly/apig/apig"
+	"github.com/wantedly/apig/util"
+)
+
+const (
+	modelDir   = "models"
+	targetFile = "main.go"
 )
 
 type GenCommand struct {
@@ -9,9 +20,17 @@ type GenCommand struct {
 }
 
 func (c *GenCommand) Run(args []string) int {
-	// Write your code here
-
-	return 0
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	if !util.FileExists(filepath.Join(wd, targetFile)) || !util.FileExists(filepath.Join(wd, modelDir)) {
+		fmt.Fprintf(os.Stderr, `%s is not project root. Please move.
+`, wd)
+		return 1
+	}
+	return apig.Generate(wd, modelDir, targetFile)
 }
 
 func (c *GenCommand) Synopsis() string {
