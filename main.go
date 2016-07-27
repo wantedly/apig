9b4e94700aceb37f93160a07bd1f48b94c65e3a2
munisystem <munisystem@gmail.com>
2016-07-27 20:18:33 +0900

    Fix not to run go generate

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "os"
 
-// go:generate go-bindata -o ./apig/bindate.go -pkg apig _templates/...
+//go:generate go-bindata -o ./apig/bindata.go -pkg apig _templates/...
 
 func main() {
 	os.Exit(Run(os.Args[1:]))
