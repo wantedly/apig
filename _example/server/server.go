5425847f38163f095cf0a07d4ad7952f1e9ce8ae
munisystem <munisystem@gmail.com>
2016-07-28 16:13:34 +0900

    Fix format

--- _example/server/server.go
+++ _example/server/server.go
@@ -4,14 +4,14 @@ import (
 	"github.com/wantedly/apig/_example/middleware"
 	"github.com/wantedly/apig/_example/router"
 
-  "github.com/gin-gonic/gin"
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func Setup(db *gorm.DB) *gin.Engine {
-  r := gin.Default()
-  r.Use(middleware.SetDBtoContext(db))
-  router.Initialize(r)
-  return r
+	r := gin.Default()
+	r.Use(middleware.SetDBtoContext(db))
+	router.Initialize(r)
+	return r
 }
