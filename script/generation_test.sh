58395956aaac9695acb9f2ebdc729c20f19243d0
Daisuke Fujita <dtanshi45@gmail.com>
2016-08-04 11:49:16 +0900

    Use double brackets in generation test

--- script/generation_test.sh
+++ script/generation_test.sh
@@ -6,7 +6,7 @@ echo "===> Generating API server..."
 cd _example
 ../bin/apig gen --all
 
-if [ ! $(git status | grep 'nothing to commit') ]; then
+if [[ ! $(git status | grep 'nothing to commit') ]]; then
   echo " x Generator artifact and example application are different."
   git --no-pager diff
   exit 1
@@ -16,9 +16,10 @@ echo "===> Building API server..."
 go get ./...
 go build
 
-if [ $? -gt 0 ]; then
+if [[ $? -gt 0 ]]; then
   echo " x Failed to build generated API server."
   exit 1
 fi
 
 echo " o Generation test PASSED!"
+q
