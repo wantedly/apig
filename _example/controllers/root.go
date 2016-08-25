f43f7cdccdb145f3c031ca8b43921e2ac5a8484d
Go Takagi <takagi@shimastripe.com>
2016-08-25 16:23:41 +0900

    Fit to master example

--- _example/controllers/root.go
+++ _example/controllers/root.go
@@ -20,16 +20,16 @@ func APIEndpoints(c *gin.Context) {
 	baseURL := fmt.Sprintf("%s://%s", reqScheme, reqHost)
 
 	resources := map[string]string{
-		"companies_url": baseURL + "/companies",
-		"company_url":   baseURL + "/companies/{id}",
-		"emails_url":    baseURL + "/emails",
-		"email_url":     baseURL + "/emails/{id}",
-		"jobs_url":      baseURL + "/jobs",
-		"job_url":       baseURL + "/jobs/{id}",
-		"profiles_url":  baseURL + "/profiles",
-		"profile_url":   baseURL + "/profiles/{id}",
-		"users_url":     baseURL + "/users",
-		"user_url":      baseURL + "/users/{id}",
+		"companies_url": baseURL + "/api/companies",
+		"company_url":   baseURL + "/api/companies/{id}",
+		"emails_url":    baseURL + "/api/emails",
+		"email_url":     baseURL + "/api/emails/{id}",
+		"jobs_url":      baseURL + "/api/jobs",
+		"job_url":       baseURL + "/api/jobs/{id}",
+		"profiles_url":  baseURL + "/api/profiles",
+		"profile_url":   baseURL + "/api/profiles/{id}",
+		"users_url":     baseURL + "/api/users",
+		"user_url":      baseURL + "/api/users/{id}",
 	}
 
 	c.IndentedJSON(http.StatusOK, resources)
