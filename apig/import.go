b4f7496201501b35b7fb1c22b1aba68e6f2cf033
Go Takagi <takagi@shimastripe.com>
2016-09-12 19:00:39 +0900

    Except path

--- apig/import.go
+++ apig/import.go
@@ -7,9 +7,11 @@ func formatImportDir(paths []string) []string {
 	flag := map[string]bool{}
 	for i := 0; i < len(paths); i++ {
 		dir := filepath.Dir(paths[i])
-		if !flag[dir] {
-			flag[dir] = true
-			results = append(results, dir)
+		if dir != "." {
+			if !flag[dir] {
+				flag[dir] = true
+				results = append(results, dir)
+			}
 		}
 	}
 	return results
